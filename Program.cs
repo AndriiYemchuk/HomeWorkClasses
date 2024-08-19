bdb93abc1cf8cacccec830aff17f41552c00c979
Andrii <117695730+AndriiYemchuk@users.noreply.github.com>
2024-08-19 16:34:26 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,43 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using System;
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+Employee person1 = new Employee();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+Console.Write("Введіть імʼя співробітника: ");
+person1.name = Console.ReadLine();
+Console.Write("Введіть прізвище співробітника: ");
+person1.surname = Console.ReadLine();
+
+
+if (person1.name == "Андрій"&&person1.surname == "Ємчук")
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    person1.expirience = 2;
+    person1.role = "senior";
+    person1.salary = 40000;
+    person1.wage = (5/100)*person1.salary;
+    person1.tax = (18/100)*person1.salary;
+    person1.Print();
+}
+else
+{
+    Console.WriteLine("Співробітника не знайдено!");
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
+public class Employee
+{
 
-app.UseAuthorization();
+    public string name = "Unknown";
+    public string surname = "Unknown";
+    public int expirience = 0;
+    public string role = "junior";
+    public float salary = 0;
+    public float wage = 0;
+    public float tax = 0;
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    public void Print()
+    {
+        Console.WriteLine($"Імʼя: {name} Прізвище: {surname} Років досвіду: {expirience} Посада: {role} Оклад: {wage}грн. Податок: {tax}грн.");
+    }
 
-app.Run();
-
+}
