88c9fe3577ee2d49fb425935ae2bfabacef9f9ff
Andrii <117695730+AndriiYemchuk@users.noreply.github.com>
2024-08-19 16:07:13 +0200

    Add files via upload

--- User.cs
+++ User.cs
@@ -1,9 +1,31 @@
-using System;
-namespace Samples.Purchasing.AppleAppStore.FraudDetection
+﻿using System;
+
+
+
+User user = new User();
+
+user.name = "Andrii";
+user.login = "yemchukandrii@knute.edu.com";
+user.surnom = "Yemchuk";
+user.yearsold = 19;
+
+user.Print();
+
+public class User
 {
-    [Serializable]
-    public class User
+    public string date_of_registration = "19.08.2024";
+    public string name = "Not Found";
+    public string login = "Enter Login";
+    public string surnom = "Not Found";
+    public int yearsold;
+    public string Date
     {
-        public string Username;
+
+        get { return date_of_registration; }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Name: {name} Surname: {surnom} Login: {login} Years: {yearsold} You're with us since: {Date}");
     }
 }
